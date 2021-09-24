91c7dd300213957bc26407a3f991ff474a990e5e
Paloma <paloma.arevalog@gmail.com>
2021-09-24 10:55:55 +0200

    list in react and add new club

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,70 @@
 // Fichero src/components/App.js
 import '../styles/App.scss';
 import initialData from '../data/clubs.json';
+import { useState } from 'react';
 
 function App() {
+	const [data, setData] = useState(initialData);
+	const [name, setName] = useState('');
+	const [openOnWeekdays, setOpenOnWeekdays] = useState('');
+	const [openOnWeekend, setOpenOnWeekend] = useState('');
+	const [newName, setNewName] = useState('');
+	const [newWeekdays, setNewWeekdays] = useState('');
+	const [newWeekend, setNewWeekend] = useState('');
+
+	const handleNewName = (ev) => {
+		setNewName(ev.currentTarget.value);
+		setName(ev.target.value);
+	};
+	const handleNewWeekdays = (ev) => {
+		setNewWeekdays(ev.currentTarget.value);
+		setOpenOnWeekdays(ev.target.value);
+	};
+	const handleNewWeekend = (ev) => {
+		setNewWeekend(ev.currentTarget.value);
+		setOpenOnWeekend(ev.target.value);
+	};
+
+	const handleClick = (ev) => {
+		ev.preventDefault();
+
+		const newClub = {
+			name: newName,
+			openOnWeekdays: newWeekdays,
+			openOnWeekend: newWeekend,
+		};
+		setData([...data, newClub]);
+		setNewName('');
+		setNewWeekdays('');
+		setNewWeekend('');
+	};
+	//render html
+
+	const htmlClubList = data.map((oneClub, index) => (
+		<li key={index} className="club__item">
+			<p className="club__name">
+				<label className="club__label">
+					{`#${index}: ${oneClub.name}`}
+				</label>
+			</p>
+			<p className="club__openWeekdays">
+				<label className="club__label">
+					{`Abierto entre semana:${
+						oneClub.openOnWeekdays ? `Si` : `No`
+					}`}
+				</label>
+			</p>
+			<p className="club__openWeekend">
+				<label className="club__label">
+					{`Abierto el fin de semana:${
+						oneClub.openOnWeekend ? `Si` : `No`
+					}`}
+				</label>
+			</p>
+		</li>
+	));
 	return (
 		<div>
-			{' '}
 			{/* header */}
 			<header className="header">
 				<h1 className="header__title">Mis clubs</h1>
@@ -14,75 +73,45 @@ function App() {
 				</form> */}
 			</header>
 			<main>
-				{/* club list */}
-				<ul className="club__list">
-					<li className="club__item">
-						<p className="club__name">
-							<label className="club__label">#0: Book club</label>
-						</p>
-						<p className="club__openLabour">
-							<label className="club__label">
-								Abierto entre semana:{' '}
-							</label>
-						</p>
-						<p className="club__openWeekend">
-							<label className="club__label">
-								Abierto el fin de semana:{' '}
-							</label>
-						</p>
-					</li>
-					<li className="club__item">
-						<p className="club__name">
-							<label className="club__label">
-								#1: Chess club
-							</label>
-						</p>
-						<p className="club__openLabour">
-							<label className="club__label">
-								Abierto entre semana:{' '}
-							</label>
-						</p>
-						<p className="club__openWeekend">
-							<label className="club__label">
-								Abierto el fin de semana:{' '}
-							</label>
-						</p>
-					</li>{' '}
-					<li className="club__item">
-						<p className="club__name">
-							<label className="club__label">
-								#2: Escape room club
-							</label>
-						</p>
-						<p className="club__openLabour">
-							<label className="club__label">
-								Abierto entre semana:{' '}
-							</label>
-						</p>
-						<p className="club__openWeekend">
-							<label className="club__label">
-								Abierto el fin de semana:{' '}
-							</label>
-						</p>
-					</li>{' '}
-					<li className="club__item">
-						<p className="club__name">
-							<label className="club__label">
-								#3: Debate club
-							</label>
-						</p>
-						<p className="club__openLabour">
-							<label className="club__label">
-								Abierto entre semana:{' '}
-							</label>
-						</p>
-						<p className="club__openWeekend">
-							<label className="club__label">
-								Abierto el fin de semana:{' '}
-							</label>
-						</p>
-					</li>
-				</ul>
+				<ul>{htmlClubList}</ul>
+
+				<form className="new-contact__form">
+					<h2 className="new-contact__title">Añade un nuevo club</h2>
+					Nombre{' '}
+					<input
+						className="new-club__input"
+						type="text"
+						name="name"
+						id="name"
+						placeholder="Nombre"
+						onChange={handleNewName}
+						value={newName}
+					/>
+					¿Abre entre semana?
+					<input
+						className="new-club__input"
+						type="radio"
+						name="weekdays"
+						id="weekdays"
+						onChange={handleNewWeekdays}
+						checked={newWeekdays ? true : false}
+					/>
+					¿Abre los fines de semana?
+					<input
+						className="new-club__input"
+						type="radio"
+						name="weekend"
+						id="weekend"
+						onChange={handleNewWeekend}
+						checked={newWeekend ? true : false}
+					/>
+					<input
+						className="new-contact__btn"
+						type="submit"
+						value="Añadir"
+						onClick={handleClick}
+					/>
+				</form>
 			</main>
 		</div>
 	);
